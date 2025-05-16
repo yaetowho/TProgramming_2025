f90e7e812d3272ba73391eca6fa601c47e096800
yaetowho <yaetowho12301@gmail.com>
2025-05-17 00:02:01 +0300

    Добавлена задача А

--- main.go
+++ main.go
@@ -6,15 +6,23 @@ import (
 )
 
 func main() {
-	fmt.Println("Журавлев Сергей Викторович")
-	fmt.Println("Значения функции arcsin(x^a) + arccos(x^b):")
-	fmt.Println(calculate(2, 3, 0.08))
+	//   fmt.Println("Журавлев Сергей Викторович")
+	//   fmt.Println("Значения функции arcsin(x^a) + arccos(x^b):", calculate(2, 3, 0.08))
+	fmt.Println("Задача А:")
+	taskA()
 }
 
 func calculate(a, b, x float64) float64 {
 	arcsin := math.Asin(math.Pow(x, a))
 	arccos := math.Acos(math.Pow(x, b))
 	result := arcsin + arccos
-
 	return result
+}
+
+func taskA() {
+	xn, xe, dx := 0.11, 0.36, 0.05
+
+	for x := xn; x < xe; x += dx {
+		fmt.Println(calculate(2.0, 3.0, x))
+	}
 }
