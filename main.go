5821cb745e85ffcbc2b3dcf9e505e3c0f29dcc4d
yaetowho <yaetowho12301@gmail.com>
2025-05-17 00:47:54 +0300

    Исправление для request

--- main.go
+++ main.go
@@ -7,8 +7,7 @@ import (
 
 func main() {
 	fmt.Println("Журавлев Сергей Викторович")
-	fmt.Println("Значения функции arcsin(x^a) + arccos(x^b):")
-	fmt.Println(calculate(2, 3, 0.08))
+	fmt.Println("Значения функции arcsin(x^a) + arccos(x^b):", calculate(2, 3, 0.08))
 }
 
 func calculate(a, b, x float64) float64 {
