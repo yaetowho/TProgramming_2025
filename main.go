f3819ddbf05720ba08917ebfff3d6115b9f7df8e
yaetowho <yaetowho12301@gmail.com>
2025-05-16 14:34:57 +0300

    Удалено дублируемое

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println(calculate(2, 3, 0.08))
 }
 
-func calculate(a, b, x float64) (float64 float64) {
+func calculate(a, b, x float64) float64 {
 	arcsin := math.Asin(math.Pow(x, a))
 	arccos := math.Acos(math.Pow(x, b))
 	result := arcsin + arccos
