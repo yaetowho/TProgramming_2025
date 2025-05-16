4bb7cd7f76561339fb7f55f02accfb30ea606f6e
yaetowho <yaetowho12301@gmail.com>
2025-05-16 14:25:45 +0300

    Math function task

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Журавлев Сергей Викторович")
+	fmt.Println("Значения функции arcsin(x^a) + arccos(x^b):")
+	fmt.Println(calculate(2, 3, 0.8))
+}
+
+func calculate(a, b, x float64) (float64 float64) {
+	arcsin := math.Asin(math.Pow(x, a))
+	arccos := math.Acos(math.Pow(x, b))
+	result := arcsin + arccos
+
+	return result
 }
